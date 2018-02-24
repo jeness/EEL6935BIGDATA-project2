3d72a810cfb5c116c2672f00389db1787ede5db0
jeness <yuhaoran9968@gmail.com>
2018-02-24 01:11:34 -0500

    2nd version

--- q4_sentiment.py
+++ q4_sentiment.py
@@ -11,8 +11,8 @@ from q4_softmaxreg import softmaxRegression, getSentenceFeature, accuracy, softm
 REGULARIZATION = None   # Assign a list of floats in the block below
 ### YOUR CODE HERE
 REGULARIZATION = np.logspace(-6,0.1,21)
-REGULARIZATIOn = np.hstack([0,REGULARIZATION])
-REGULARIZATION = [0.1,0.3,0.01,0.03,0.001,0.003,0.0001,0.0003,0.00001,0.00003]
+# REGULARIZATIOn = np.hstack([0,REGULARIZATION])
+# REGULARIZATION = [0.1,0.3,0.01,0.03,0.001,0.003,0.0001,0.0003,0.00001,0.00003]
 # Load the dataset
 dataset = StanfordSentiment()
 tokens = dataset.tokens()
